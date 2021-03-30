645121f7e3b0b4d018dc3ac0ade5fb404cb8aa32
Oksana Akhadova <o.akhadova@gmail.com>
2021-03-30 12:13:57 +0300

    changed smokeFunctional (used function)

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,5 +1,6 @@
 import sel from '../../data/selectors';
 import {name, gender, age, story} from '../../data/testData';
+import inputValues4 from '../../helpers/methods';
 
 describe('Required fields and story created', function () {
 
@@ -19,13 +20,8 @@ describe('Required fields and story created', function () {
 
     it('TC-027 User can create a story with valid values', function () {
         browser.refresh();
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
-        $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        inputValues4(name.default, gender.she, age.default, story.comedy);
         $(sel.submit).click();
-
         let tryAgainBtn = $(sel.tryAgain).isDisplayed();
         expect(tryAgainBtn).toEqual(true);
     });
